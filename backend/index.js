4457ac92b0e7959db23bdd5d9e9cb0ebfac7c983
Cinthia Rodriguez <599034+cinthiaro@users.noreply.github.com>
2025-04-26 15:57:06 -0600

    Agregar comentarios al index.js

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const cors = require("cors");
+
+//2. Cargar variables de entorno
 require("dotenv").config();
 
+// 1. Levantar express
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 const port = 3000;
 
+// 3. Cargar rutas
 const authRoutes = require("./routes/auth");
 app.use("/auth", authRoutes);
 
